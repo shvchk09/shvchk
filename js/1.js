cfa93052b8535aaa0c0db83ab6903e0afb372352
shvchk09 <madina7783617072@gmail.com>
2025-12-19 20:47:05 +0500

    Add files via upload

---
+++ js/1.js
@@ -0,0 +1,11 @@
+const blocks = document.querySelectorAll('.block');
+
+const observer = new IntersectionObserver(entries => {
+  entries.forEach(entry => {
+    if(entry.isIntersecting){
+      entry.target.classList.add('visible');
+    }
+  });
+}, { threshold: 0.1 });
+
+blocks.forEach(block => observer.observe(block));
